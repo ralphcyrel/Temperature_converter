50cf762fff70d85bab2fc7e4f410261d2da2acf3
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-09-25 11:05:58 +0800

    celsius

--- temperature_converter.py
+++ temperature_converter.py
@@ -2,5 +2,8 @@
 while True:
     temp_choice = input("Type 'C' to convert Celsius to Fahrenheit and Type 'F' to convert Fahrenheit to Celsius: ")
 #will ask the user to input temperature for conversion
-
+    if temp_choice == 'C':
+        celsius = float(input("Enter the Temperature in Celsius: "))
+        fahrenheit = celsius * 9 / 5 + 32
+        print("The Temperature in Fahrenheit is:", fahrenheit)
 #will ask the user to try again
