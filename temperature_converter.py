c9a40e3147cad4818d4339a399d88a4db86d40cd
Ralph Cyrel B. Ronda <justinbebet12@gmail.com>
2024-09-25 11:04:25 +0800

    will ask the user to input C or F for conversion

--- temperature_converter.py
+++ temperature_converter.py
@@ -1,5 +1,6 @@
 #will ask the user to choose temperature C or F
 while True:
-    input("Type 'C' to convert Celsius to Fahrenheit and Type 'F' to convert Fahrenheit to Celsius: ")
+    temp_choice = input("Type 'C' to convert Celsius to Fahrenheit and Type 'F' to convert Fahrenheit to Celsius: ")
 #will ask the user to input temperature for conversion
+
 #will ask the user to try again
